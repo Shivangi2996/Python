0be51183b179497ce147560a4266243d7c8c415a
Shivangi Vidyarthi <59414574+Shivangi2996@users.noreply.github.com>
2022-08-30 18:39:27 +0530

    Create Prime_number_before_inputNumber.py

---
+++ Prime_number_before_inputNumber.py
@@ -0,0 +1,9 @@
+N = int(input('Please enter a integer no: '))
+for n in range(1,N+1):
+    count =0
+    for i in range(2,(n//2+1)):
+        if (n%i)==0:
+            count+=1
+            break;
+    if (count==0 and n!=1):
+        print("%d " %n,end='')
